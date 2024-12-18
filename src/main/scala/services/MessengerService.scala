6dd19da4e5b5648ef8725f1bae0a68d8bf9ffc11
JugglypuffM <stas.klinin@gmail.com>
2024-12-18 15:27:07 +0500

    feat: реализована логика сервиса отправки сообщений

---
+++ src/main/scala/services/MessengerService.scala
@@ -0,0 +1,44 @@
+package services
+
+import cats.effect.IO
+import domain.repositories.{Message, User}
+import org.http4s.{EntityDecoder, Request}
+import repositories.message.MessageRepository
+import repositories.user.UserRepository
+import services.MessengerService.{InvalidTokenException, InvalidUserException, JSONParseException}
+
+class MessengerService(userRepository: UserRepository[IO], messageRepository: MessageRepository[IO]) {
+  def parseUserData[A](request: Request[IO])(implicit decoder: EntityDecoder[IO, A]): IO[A] = request
+    .as[A]
+    .handleErrorWith(_ => IO.raiseError(JSONParseException("Failed to parse JSON")))
+
+  def validateAndGetUser(login: String): IO[User] =
+    for {
+      userOption <- userRepository.getUserByLogin(login)
+      user       <- IO.fromOption(userOption)(InvalidUserException(s"User $login not found"))
+    } yield user
+
+  def validateToken(user: User, token: String): IO[Unit] =
+    if (user.oauthToken != token) IO.raiseError(InvalidTokenException("Invalid token provided"))
+    else IO(())
+
+  def getUnreadMessages(sender: String, recipient: String): IO[List[String]] =
+    for {
+      messages        <- messageRepository.getUnreadMessagesFrom(sender, recipient)
+      _               <- messageRepository.deleteUnreadMessages(recipient)
+    } yield messages
+
+  def sendMessage(sender: String, recipient: String, content: String): IO[Unit] =
+    for {
+      message <- IO(Message(sender, recipient, content))
+      _       <- messageRepository.saveMessage(message)
+    } yield ()
+}
+
+object MessengerService {
+  class InvalidUserException(message: String) extends RuntimeException(message)
+
+  class InvalidTokenException(message: String) extends RuntimeException(message)
+
+  class JSONParseException(message: String) extends RuntimeException(message)
+}
